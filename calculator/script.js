cad1afffbe66564b60310f3da3cf92fff83767a2
Alexey Teterin <teterin.alexey@gmail.com>
2020-09-22 21:28:42 +0500

    refactor: apply eslint

--- calculator/script.js
+++ calculator/script.js
@@ -6,8 +6,8 @@ class Calculator {
   }
 
   clear() {
-    this.currOperand = "";
-    this.prevOperand = "";
+    this.currOperand = '';
+    this.prevOperand = '';
     this.operation = undefined;
   }
 
@@ -16,46 +16,42 @@ class Calculator {
   }
 
   appendNumber(number) {
-    if (number === "." && this.currOperand.includes(".")) return;
-    if (this.currOperand === "0") this.currOperand = number.toString();
+    if (number === '.' && this.currOperand.includes('.')) return;
+    if (this.currOperand === '0') this.currOperand = number.toString();
     else this.currOperand = this.currOperand.toString() + number.toString();
   }
 
   chooseOperation(operation) {
-    if (this.operation !== undefined &&
-      this.prevOperand !== "" &&
-      this.currOperand !== "")
-      this.compute();
+    if (this.operation !== undefined && this.prevOperand !== '' && this.currOperand !== '') this.compute();
     this.operation = operation;
-    if (this.currOperand === "") {
+    if (this.currOperand === '') {
       this.prevOperand = this.prevOperand.slice(0, -1) + operation.toString();
     } else {
-      this.prevOperand = this.currOperand.toString() + " " + operation.toString();
-      this.currOperand = "";
+      this.prevOperand = `${this.currOperand.toString()} ${operation.toString()}`;
+      this.currOperand = '';
     }
   }
 
   compute() {
     switch (this.operation) {
-      case "+":
-        this.currOperand =
-          parseFloat(this.prevOperand) + parseFloat(this.currOperand);
+      case '+':
+        this.currOperand = parseFloat(this.prevOperand) + parseFloat(this.currOperand);
         break;
-      case "-":
+      case '-':
         this.currOperand = parseFloat(this.prevOperand) - parseFloat(this.currOperand);
         break;
-      case "/":
-        this.currOperand =
-          parseFloat(this.prevOperand) / parseFloat(this.currOperand);
+      case '/':
+        this.currOperand = parseFloat(this.prevOperand) / parseFloat(this.currOperand);
         break;
-      case "*":
-        this.currOperand =
-          parseFloat(this.prevOperand) * parseFloat(this.currOperand);
+      case '*':
+        this.currOperand = parseFloat(this.prevOperand) * parseFloat(this.currOperand);
+        break;
+      default:
         break;
     }
-    this.prevOperand = "";
+    this.prevOperand = '';
 
-    //TODO: вместить длинные числа
+    // TODO: вместить длинные числа
   }
 
   updateDisplay() {
@@ -64,41 +60,42 @@ class Calculator {
   }
 }
 
-const NUM_BUTTONS = document.querySelectorAll(".calc__num");
-const OPERATION_BUTTONS = document.querySelectorAll(".calc__operation");
-const EQUALS_BUTTON = document.querySelector(".calc__equals");
-const DELETE_BUTTON = document.querySelector(".calc__del");
-const AC_BUTTON = document.querySelector(".calc__ac");
-const PREV_OPERAND = document.querySelector(".prev-operand");
-const CURR_OPERAND = document.querySelector(".curr-operand");
+const NUM_BUTTONS = document.querySelectorAll('.calc__num');
+const OPERATION_BUTTONS = document.querySelectorAll('.calc__operation');
+const EQUALS_BUTTON = document.querySelector('.calc__equals');
+const DELETE_BUTTON = document.querySelector('.calc__del');
+const AC_BUTTON = document.querySelector('.calc__ac');
+const PREV_OPERAND = document.querySelector('.prev-operand');
+const CURR_OPERAND = document.querySelector('.curr-operand');
 
 const CALCULATOR = new Calculator(PREV_OPERAND, CURR_OPERAND);
 
 NUM_BUTTONS.forEach((btn) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener('click', () => {
     CALCULATOR.appendNumber(btn.innerText);
     CALCULATOR.updateDisplay();
   });
 });
 
-AC_BUTTON.addEventListener("click", () => {
+AC_BUTTON.addEventListener('click', () => {
   CALCULATOR.clear();
   CALCULATOR.updateDisplay();
 });
 
-DELETE_BUTTON.addEventListener("click", () => {
+DELETE_BUTTON.addEventListener('click', () => {
   CALCULATOR.delete();
   CALCULATOR.updateDisplay();
 });
 
 OPERATION_BUTTONS.forEach((btn) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener('click', () => {
     CALCULATOR.chooseOperation(btn.innerText);
     CALCULATOR.updateDisplay();
   });
 });
 
-EQUALS_BUTTON.addEventListener("click", () => {
+EQUALS_BUTTON.addEventListener('click', () => {
   CALCULATOR.compute();
+
   CALCULATOR.updateDisplay();
 });
