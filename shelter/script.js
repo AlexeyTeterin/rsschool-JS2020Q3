c13a090ef6ff92c1a134c67bd00d0427fe2a4840
AlexeyTeterin <teterin.alexey@gmail.com>
2020-10-19 14:33:38 +0500

    feat: add popup window open and close

--- shelter/script.js
+++ shelter/script.js
@@ -1,6 +1,8 @@
 const SLIDER_CARDS = Array.from(document.getElementsByClassName('card'));
 const SLIDER_BTNS = Array.from(document.getElementsByClassName('arrow'));
 const PETS = [];
+const POPUP = document.getElementById('popup');
+const POPUP_CLOSE_BTN = document.getElementById('popup__close-btn');
 
 const request = new XMLHttpRequest();
 request.open('GET', '/our-pets/pets.json');
@@ -55,4 +57,40 @@ SLIDER_BTNS.forEach((btn) => {
   btn.addEventListener('click', () => {
     shuffleCards();
   })
+})
+
+// Show pet info
+showPetInfo = (petName) => {
+  let currentPet = {};
+
+  PETS.forEach((pet) => {
+    if (pet.name === petName) {
+      currentPet = pet;
+    }
+  })
+  console.log(currentPet);
+
+  POPUP.style.setProperty('height', document.querySelector('body').scrollHeight + 'px');
+  POPUP.classList.remove('hidden');
+
+}
+
+// Selecting card to show its info
+SLIDER_CARDS.forEach((card) => {
+  card.addEventListener('click', () => {
+    showPetInfo(card.children[1].innerText);
+  })
+})
+
+hidePetInfo = () => {
+  POPUP.classList.add('hidden');
+}
+
+POPUP_CLOSE_BTN.addEventListener('click', () => {
+  hidePetInfo();
+})
+
+POPUP.addEventListener('click', (event) => {
+  if (event.target.id === 'popup__container') return;
+  else hidePetInfo();
 })
