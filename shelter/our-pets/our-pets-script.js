36c4ebd7c35e22caffbb20bba62298d9159cb525
AlexeyTeterin <teterin.alexey@gmail.com>
2020-10-20 15:40:44 +0500

    feat: add ALL_48_PETS array

--- shelter/our-pets/our-pets-script.js
+++ shelter/our-pets/our-pets-script.js
@@ -1,16 +1,19 @@
 const PETS = [];
+const VISIBLE_PETS = document.querySelector('.pets');
+const ALL_48_PETS = [];
 
 const request = new XMLHttpRequest();
 request.open('GET', 'pets.json');
 request.responseType = 'json';
 request.send();
 
-
 request.onload = () => {
   const pets = request.response;
   for (const pet of pets) {
     PETS.push(pet);
   }
+  for (let i = 0; i < 6; i += 1) {
+    ALL_48_PETS.push(...pets);
+  }
 }
 
-// const LOGO1 = document.querySelectorAll('.logo');
