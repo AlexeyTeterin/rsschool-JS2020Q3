e767c3c3f7cc3b9f5246959aa4ee67c1ecf45411
AlexeyTeterin <teterin.alexey@gmail.com>
2020-10-22 17:15:01 +0500

    fix: burger menu funcs refactor: add burger app

--- shelter/burger.js
+++ shelter/burger.js
@@ -5,57 +5,56 @@ const MENU_ELEMENTS = Array.from(document.getElementsByClassName('menu-link'));
 const MENU_ELEMENTS_DISABLED = Array.from(document.getElementsByClassName('disabled'));
 const LOGO = document.getElementsByClassName('logo')[0];
 const LOGO2 = document.getElementsByClassName('logo2')[0];
-const HEADER = document.getElementsByTagName('header')[0];
 
-hideMenu = () => {
-  MENU.classList.add('header__menu_hide');
-  BURGER.classList.remove('burger__img_active');
-  setTimeout(() => {
-    MENU.classList.remove('header__menu_show');
-  }, 250);
-  menuBlock.classList.add('hidden');
-  LOGO.classList.remove('hidden');
-  LOGO2.classList.add('hidden');
-}
+const BURGER_APP = {
+  hideMenu() {
+    MENU.classList.add('header__menu_hide');
+    BURGER.classList.remove('burger__img_active');
+    setTimeout(() => {
+      MENU.classList.remove('header__menu_show');
+    }, 250);
+    menuBlock.classList.add('hidden');
+    LOGO.classList.remove('hidden');
+    LOGO2.classList.add('hidden');
+  },
 
-showMenu = () => {
-  MENU.classList.add('header__menu_show');
-  BURGER.classList.add('burger__img_active');
-  setTimeout(() => {
-    MENU.classList.remove('header__menu_hide');
-  }, 10);
-  menuBlock.style.setProperty('height', document.querySelector('body').scrollHeight + 'px');
-  menuBlock.classList.remove('hidden');
-  LOGO.classList.add('hidden');
-  LOGO2.classList.remove('hidden');
-}
+  showMenu() {
+    MENU.classList.add('header__menu_show');
+    BURGER.classList.add('burger__img_active');
+    setTimeout(() => {
+      MENU.classList.remove('header__menu_hide');
+    }, 10);
+    menuBlock.classList.remove('hidden');
+    LOGO.classList.add('hidden');
+    LOGO2.classList.remove('hidden');
+  },
+};
 
 BURGER.addEventListener('click', () => {
   if (BURGER.classList.contains('burger__img_active')) {
-    hideMenu();
+    BURGER_APP.hideMenu();
   } else {
-    showMenu();
+    BURGER_APP.showMenu();
   }
-})
+});
 
 // On window resize close burger menu
-window.onresize = (event) => {
-  hideMenu();
+window.onresize = () => {
+  BURGER_APP.hideMenu();
 };
 
 MENU_ELEMENTS_DISABLED.forEach((a) => {
-  a.addEventListener('click', () => {
-    return false;
-  })
-})
+  a.addEventListener('click', () => false);
+});
 
 // Close menu on li click
 MENU_ELEMENTS.forEach((a) => {
   a.addEventListener('click', () => {
     if (a.classList.contains('disabled')) {
       return false;
-    } else setTimeout(() => {
-      hideMenu();
+    } setTimeout(() => {
+      BURGER_APP.hideMenu();
     }, 50);
-  })
-})
+    return true;
+  });
+});
