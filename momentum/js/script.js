afac00c6652667bbc3c8f222bd35493626a02e80
AlexeyTeterin <teterin.alexey@gmail.com>
2020-10-24 13:49:13 +0500

    refactor : const APP with methods

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -5,99 +5,98 @@ const GREETING = document.getElementById('greeting');
 const NAME = document.getElementById('name');
 const GOAL = document.getElementById('goal');
 
-// Show time
-const showTime = () => {
-  const today = new Date();
-  const hour = today.getHours();
-  const min = today.getMinutes();
-  const sec = today.getSeconds();
+const APP = {
+  run() {
+    this.showTime();
+    this.showDate();
+    this.setBGGreet();
+    this.getName();
+    this.getGoal();
+  },
 
-  // Add zeros
-  const addZero = (n) => (parseInt(n, 10) < 10 ? '0' : '') + n;
+  showTime() {
+    const today = new Date();
+    const hour = today.getHours();
+    const min = today.getMinutes();
+    const sec = today.getSeconds();
 
-  // Output Time
-  TIME.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+    // Add zeros
+    const addZero = (n) => (parseInt(n, 10) < 10 ? '0' : '') + n;
 
-  setTimeout(showTime, 1000);
-};
+    // Output Time
+    TIME.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
-// SHow Date
-const showDate = () => {
-  const today = new Date();
-  const day = today.getDate();
-  const weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][today.getDay()];
-  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][today.getMonth()];
+    setTimeout(APP.showTime, 1000);
+  },
 
-  // Output Date
-  DATE.innerHTML = `${weekDay}, ${month} ${day}`;
-};
+  showDate() {
+    const today = new Date();
+    const day = today.getDate();
+    const weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][today.getDay()];
+    const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][today.getMonth()];
 
-// Set BG and Greeting
-const setBGGreet = () => {
-  const today = new Date();
-  const hour = today.getHours();
+    // Output Date
+    DATE.innerHTML = `${weekDay}, ${month} ${day}`;
+  },
 
-  if (hour < 12) {
-    document.body.style.backgroundImage = "url('../img/morning.jpg')";
-    GREETING.textContent = 'Good morning';
-  } else if (hour < 18) {
-    document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
-    GREETING.textContent = 'Good afternoon';
-  } else {
-    document.body.style.backgroundImage = "url('../img/evening.jpg')";
-    GREETING.textContent = 'Good evening';
-    document.body.style.color = 'white';
-  }
-};
+  setBGGreet() {
+    const today = new Date();
+    const hour = today.getHours();
 
-// Get Name
-const getName = () => {
-  if (localStorage.getItem('name') === null) {
-    NAME.textContent = '[Enter yor name]';
-  } else {
-    NAME.textContent = localStorage.getItem('name');
-  }
-};
-
-// Set Name
-const setName = (event) => {
-  if (event.type === 'keypress') {
-    if (event.which === 13 || event.keyCode === 13) {
-      NAME.blur();
+    if (hour < 12) {
+      document.body.style.backgroundImage = "url('../img/morning.jpg')";
+      GREETING.textContent = 'Good morning';
+    } else if (hour < 18) {
+      document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
+      GREETING.textContent = 'Good afternoon';
+    } else {
+      document.body.style.backgroundImage = "url('../img/evening.jpg')";
+      GREETING.textContent = 'Good evening';
+      document.body.style.color = 'white';
     }
-  } else if (event.target.innerText !== '') {
-    localStorage.setItem('name', event.target.innerText);
-  } else getName();
-};
+  },
 
-// Get GOAL
-const getGoal = () => {
-  if (localStorage.getItem('goal') === null) {
-    GOAL.textContent = '[Enter your goal]';
-  } else {
-    GOAL.textContent = localStorage.getItem('goal');
-  }
-};
-
-// Set Goal
-const setGoal = (event) => {
-  if (event.type === 'keypress') {
-    if (event.which === 13 || event.keyCode === 13) {
-      GOAL.blur();
+  getName() {
+    if (localStorage.getItem('name') === null) {
+      NAME.textContent = '[Enter yor name]';
+    } else {
+      NAME.textContent = localStorage.getItem('name');
     }
-  } else if (event.target.innerText !== '') {
-    localStorage.setItem('goal', event.target.innerText);
-  } else getGoal();
+  },
+
+  setName(event) {
+    if (event.type === 'keypress') {
+      if (event.which === 13 || event.keyCode === 13) {
+        NAME.blur();
+      }
+    } else if (event.target.innerText !== '') {
+      localStorage.setItem('name', event.target.innerText);
+    } else getName();
+  },
+
+  getGoal() {
+    if (localStorage.getItem('goal') === null) {
+      GOAL.textContent = '[Enter your goal]';
+    } else {
+      GOAL.textContent = localStorage.getItem('goal');
+    }
+  },
+
+  setGoal(event) {
+    if (event.type === 'keypress') {
+      if (event.which === 13 || event.keyCode === 13) {
+        GOAL.blur();
+      }
+    } else if (event.target.innerText !== '') {
+      localStorage.setItem('goal', event.target.innerText);
+    } else getGoal();
+  },
 };
 
-NAME.addEventListener('keypress', setName);
-NAME.addEventListener('blur', setName);
-GOAL.addEventListener('keypress', setGoal);
-GOAL.addEventListener('blur', setGoal);
+// Event listeners
+NAME.addEventListener('keypress', APP.setName);
+NAME.addEventListener('blur', APP.setName);
+GOAL.addEventListener('keypress', APP.setGoal);
+GOAL.addEventListener('blur', APP.setGoal);
 
-// Run
-showTime();
-showDate();
-setBGGreet();
-getName();
-getGoal();
+APP.run();
