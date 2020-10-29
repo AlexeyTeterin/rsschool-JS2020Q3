29508984291c7770080fc4ca7ce9338239d787bb
Alexey Teterin <teterin.alexey@gmail.com>
2020-10-29 21:30:01 +0500

    fix: restore lang key

--- keyboard/index.js
+++ keyboard/index.js
@@ -15,28 +15,28 @@ const KEYBOARD = {
         'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\',
         'caps', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter',
         'lshift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'uarr', 'shift',
-        'sound', 'mic', 'lctrl', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
+        'sound', 'mic', 'lctrl', 'lalt', ' ', 'alt', 'lang', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
       ],
       en: [
         '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
         'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
         'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter',
         'lshift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'uarr', 'shift',
-        'sound', 'mic', 'lctrl', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
+        'sound', 'mic', 'lctrl', 'lalt', ' ', 'alt', 'lang', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
       ],
       ruShifted: [
         'Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'backspace',
         'tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/',
         'caps', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'enter',
         'lshift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'uarr', 'shift',
-        'sound', 'mic', 'lctrl', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
+        'sound', 'mic', 'lctrl', 'lalt', ' ', 'alt', 'lang', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
       ],
       enShifted: [
         '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'backspace',
         'tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|',
         'caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'enter',
         'lshift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'uarr', 'shift',
-        'sound', 'mic', 'lctrl', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
+        'sound', 'mic', 'lctrl', 'lalt', ' ', 'alt', 'lang', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
       ],
       whichCodes: [
         192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8,
@@ -77,7 +77,7 @@ const KEYBOARD = {
 
     if (localStorage.capsLock === 'true') {
       this.toggleCapsLock();
-      document.querySelector('#Caps').classList.toggle('keyboard__key--active', this.properties.capsLock);
+      document.querySelector('#Caps').classList.add('keyboard__key--active');
     }
 
     this.langTrigger();
