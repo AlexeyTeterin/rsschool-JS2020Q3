fc22355c850401bdcb87eb4914b0001a733eb301
AlexeyTeterin <teterin.alexey@gmail.com>
2020-10-29 17:24:06 +0500

    feat: add sound btn

--- keyboard/index.js
+++ keyboard/index.js
@@ -14,34 +14,30 @@ const KEYBOARD = {
         'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
         'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\',
         'caps', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter',
-        'lshift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'uarr', 'shift',
-        'lctrl', 'lang', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
+        'lshift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'uarr', 'shift', 'sound', 'lang', 'lctrl', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
       ],
       en: [
         '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
         'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
         'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter',
-        'lshift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'uarr', 'shift',
-        'lctrl', 'lang', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
+        'lshift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'uarr', 'shift', 'sound', 'lang', 'lctrl', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
       ],
       ruShifted: [
         'Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'backspace',
         'tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/',
         'caps', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'enter',
-        'lshift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'uarr', 'shift',
-        'lctrl', 'lang', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
+        'lshift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'uarr', 'shift', 'sound', 'lang', 'lctrl', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
       ],
       enShifted: [
         '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'backspace',
         'tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|',
         'caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'enter',
-        'lshift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'uarr', 'shift',
-        'lctrl', 'lang', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
+        'lshift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'uarr', 'shift', 'sound', 'lang', 'lctrl', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr', 'on/off',
       ],
       whichCodes: [
         192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8,
         9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220,
-        20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13,
+        20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13, null,
         999, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191,
       ],
     },
@@ -52,6 +48,7 @@ const KEYBOARD = {
     capsLock: null,
     shift: false,
     english: null,
+    sound: true,
   },
 
   init() {
@@ -98,7 +95,7 @@ const KEYBOARD = {
     // Create keys
     keyLayout.forEach((key) => {
       const keyElement = document.createElement('button');
-      const insertLineBreak = ['backspace', '\\', 'enter', 'shift'].indexOf(key) !== -1;
+      const insertLineBreak = ['backspace', '\\', 'enter', 'sound'].indexOf(key) !== -1;
 
       keyElement.setAttribute('type', 'button');
       keyElement.classList.add('keyboard__key');
@@ -269,6 +266,19 @@ const KEYBOARD = {
           });
           break;
 
+        case 'sound':
+          keyElement.classList.add('keyboard__key--dark', 'sound', 'keyboard__key--activatable', 'keyboard__key--active');
+          keyElement.id = 'sound';
+          const imgSound = document.createElement('img');
+          imgSound.src = 'sound-on.svg';
+          imgSound.alt = 'sound';
+          keyElement.append(imgSound);
+
+          keyElement.addEventListener('click', () => {
+            this.toggleSound();
+          });
+          break;
+
         default:
           keyElement.textContent = key.toLowerCase();
 
@@ -300,6 +310,21 @@ const KEYBOARD = {
     KEYBOARD.elements.keysContainer.classList.remove('hidden');
     KEYBOARD.elements.main.classList.remove('goDown');
     document.querySelector('#off').classList.add('keyboard__key--active');
+  },
+
+  toggleSound() {
+    const {
+      sound
+    } = this.properties;
+    const soundBtn = document.querySelector('#sound');
+    if (sound) {
+      soundBtn.classList.remove('keyboard__key--active');
+      soundBtn.children[0].src = 'sound-off.svg';
+    } else {
+      soundBtn.classList.add('keyboard__key--active');
+      soundBtn.children[0].src = 'sound-on.svg';
+    };
+    this.properties.sound = !sound;
   },
 
   // Input from real keyboard
