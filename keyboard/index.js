83e5ba61bb48e7dcba6bdd8a026037ac45d09ab2
Alexey Teterin <teterin.alexey@gmail.com>
2020-11-01 15:45:27 +0500

    fix: speech rec

--- keyboard/index.js
+++ keyboard/index.js
@@ -1,54 +1,40 @@
 import KEYBOARD from './keyboard.js';
 
+// Create textarea
 document.body.appendChild(document.createElement('textarea'));
 const textarea = document.querySelector('textarea');
 textarea.classList.add('use-keyboard');
 textarea.setAttribute('placeholder', 'Type your text here...');
 
-// Creating new instance of KEYBOARD class
+// Creating new instance of KEYBOARD
 const keyboard = new KEYBOARD();
 keyboard.init();
+
+// Speech recognition
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const speechBtn = document.querySelector('.mic');
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = keyboard.properties.english ? 'en-US' : 'ru-RU';
 
 // Event listeners
 window.addEventListener('DOMContentLoaded', () => textarea.focus());
 textarea.addEventListener('focusin', () => keyboard.showKeyboard());
-
-// Speech recognition
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-const speechBtn = document.querySelector('.mic');
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
-recognition.lang = 'en-US';
-
-let p = document.createElement('p');
-// const words = document.querySelector('.words');
-textarea.appendChild(p);
-
-recognition.addEventListener('result', e => {
+recognition.addEventListener('result', (e) => {
   const transcript = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
     .join('');
 
-  const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
-  p.textContent = poopScript;
-
   if (e.results[0].isFinal) {
-    p = document.createElement('p');
-    textarea.appendChild(p);
+    textarea.setRangeText(transcript, textarea.selectionStart, textarea.selectionEnd, 'end');
+    textarea.setRangeText('\n', textarea.selectionStart, textarea.selectionEnd, 'end');
   }
 });
-
-// recognition.addEventListener('end', recognition.start);
 speechBtn.addEventListener('click', () => {
-  if (Array.from(speechBtn.classList).includes('keyboard__key--active')) {
-    recognition.start();
-    console.log('start recognition...');
-  } else {
-    recognition.stop();
-    console.log('stop recognition');
-  }
+  if (Array.from(speechBtn.classList).includes('keyboard__key--active')) recognition.start();
+  else recognition.stop();
 });
+recognition.addEventListener('end', recognition.start);
 
 export default textarea;
