8cd809f60898f876d115bb32ec0d543350b95f88
AlexeyTeterin <teterin.alexey@gmail.com>
2020-11-06 15:47:51 +0500

    feat : add chip movement

--- gem-puzzle/main.js
+++ gem-puzzle/main.js
@@ -10,6 +10,8 @@ class Game {
     this.chips = null;
   }
   init() {
+
+    // create game board
     this.gameBoard = document.createElement('div');
     this.gameBoard.classList.add('game-board');
     this.gameBoard.style.setProperty('grid-template-columns',
@@ -24,12 +26,24 @@ class Game {
       chip.textContent = num;
       this.gameBoard.append(chip);
     });
+
+    // add last empty chip
     const emptyChip = document.createElement('div');
-    emptyChip.classList.add('chip-empty');
+    emptyChip.classList.add('chip', 'chip-empty');
     emptyChip.textContent = 'empty';
     this.gameBoard.append(emptyChip);
+    this.getChips();
 
-    this.chips = document.querySelectorAll('.chip');
+    // moving chips event listener
+    this.chips.forEach((chip) => {
+      chip.addEventListener('click', () => {
+        this.moveChips(chip);
+      })
+    })
+  }
+
+  getChips() {
+    this.chips = Array.from(document.querySelectorAll('.chip'));
   }
 
   shuffleChips() {
@@ -42,6 +56,30 @@ class Game {
       nums.push(random);
     };
     return nums;
+  }
+
+  fillGameBoard() {
+    this.chips.forEach((chip) => {
+
+    })
+  }
+
+  moveChips(chip) {
+    const chipPos = this.chips.indexOf(chip);
+    const clickedChip = document.querySelectorAll('.chip')[chipPos];
+    const emptyChip = document.querySelector('.chip-empty');
+    const temp = emptyChip.innerHTML;
+    console.log(Math.abs(this.chips.indexOf(emptyChip) - chipPos) === this.properties.rows);
+    const chipIsMovable = () => [1, this.properties.rows].includes( Math.abs(this.chips.indexOf(emptyChip) - chipPos));
+    
+    if (chipIsMovable()) {
+      console.log('success!');
+      emptyChip.innerHTML = chip.innerHTML;
+      emptyChip.classList.remove('chip-empty');
+      clickedChip.innerHTML = temp;
+      clickedChip.classList.add('chip-empty');
+      this.getChips();
+    }
   }
 }
 
