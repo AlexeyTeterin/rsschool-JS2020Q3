9301448d4b11b3da412f6c9183ca838e7e45cbe6
Alexey Teterin <teterin.alexey@gmail.com>
2020-11-07 23:49:53 +0500

    refactor: eslint

--- gem-puzzle/main.js
+++ gem-puzzle/main.js
@@ -5,85 +5,119 @@ class Game {
     playing: false,
     movesCounter: 0,
   }
-  header = null;
-  gameBoard = null;
-  chipsNumbers = null;
-  chips = [];
-  pauseBtn = null;
-  modal = null;
+
+  header = {
+    time: null,
+    moves: null,
+    pauseBtn: null,
+  }
+
+  gameBoard = null
+
+  chipsNumbers = null
+
+  chips = []
+
+  menu = null
 
   init() {
-    // create header
+    // create header with elements
     this.header = document.createElement('header');
-    this.header.append(document.createElement('time'));
-    this.header.append(document.createElement('moves'));
-    this.pauseBtn = document.createElement('pause');
-    this.header.append(this.pauseBtn);
+    // time
+    this.header.time = document.createElement('time');
+    this.header.append(this.header.time);
+    // moves
+    this.header.moves = document.createElement('div');
+    this.header.moves.classList.add('moves');
+    this.header.append(this.header.moves);
+    // pauseBtn
+    this.header.pauseBtn = document.createElement('div');
+    this.header.pauseBtn.classList.add('pause');
+    this.header.pauseBtn.textContent = this.properties.playing ? 'Pause game' : 'Resume game';
+    this.header.pauseBtn.addEventListener('click', () => this.setTimer(this.properties.playing ? 'off' : 'on'));
+    this.header.append(this.header.pauseBtn);
     document.body.append(this.header);
-    this.updateHeader();
-
-    // pause button
-    this.pauseBtn.textContent = this.properties.playing ? 'Pause' : 'Play';
-    this.pauseBtn.addEventListener('click', () => this.setTimer(this.properties.playing ? 'off' : 'on'));
+    this.updateHeader(0, 0);
 
     // create game board
     this.gameBoard = document.createElement('div');
     this.gameBoard.classList.add('game-board');
     this.gameBoard.style.setProperty('grid-template-columns',
-      `repeat(${this.properties.rows}, 1fr)`)
+      `repeat(${this.properties.rows}, 1fr)`);
     document.body.append(this.gameBoard);
 
-    // generate random chips
-    this.chipsNumbers = this.randomizeChips();
-    this.createChips();
-    this.fillChips(this.chipsNumbers);
-    this.getChips();
+    // create menu
+    this.menu = document.createElement('div');
+    this.menu.classList.add('menu');
+    this.menu.innerHTML = `<ul class="menu-ul">
+      <li class="menu-li btn-newGame">New game</li>
+      <li class="menu-li btn-saveGame">Save game</li>
+      <li class="menu-li btn-loadGame">Load game</li>
+      <li class="menu-li btn-scores">Scores</li>
+      <li class="menu-li btn-settings">Settings</li>
+      </ul>`;
+    this.gameBoard.append(this.menu);
+
+    document.querySelector('.btn-newGame').addEventListener('click', () => this.newGame());
   }
 
-  setTimer(switcher) {
-    const tick = () => {
-      this.properties.timer += 1;
-      this.updateHeader();
-    }
+  newGame() {
+    // generate random chips
+    this.chipsNumbers = this.randomizeChips();
+    // create chips boxes
+    this.createChips();
+    // fill chips with numbers
+    this.fillChips(this.chipsNumbers);
+    // save chips to game state
+    this.getChips();
+    // clear timer & moves
+    this.updateHeader(0, 0);
+    // hide menu
+    this.hideMenu();
+  }
 
-    if (switcher === 'on') {
-      this.properties.playing = true;
-      this.pauseBtn.textContent = 'Pause';
-      window.timer = window.setInterval(tick, 1000);
-    } else if (switcher === 'off') {
-      this.properties.playing = false;
-      this.pauseBtn.textContent = 'Play';
-      window.clearInterval(timer);
-    }
+  hideMenu() {
+    this.menu.classList.add('hidden');
+  }
+
+  showMenu() {
+    this.menu.classList.remove('hidden');
   }
 
   createChips() {
+    // clear existing chips
+    document.querySelectorAll('.chip').forEach((chip) => chip.remove());
+    this.chips = [];
+
     this.chipsNumbers.forEach(() => {
       const chip = document.createElement('div');
       chip.classList.add('chip');
       this.gameBoard.append(chip);
       this.chips.push(chip);
-    })
+    });
 
     // chips movement event listener
-    this.chips.forEach((chip) =>
-      chip.addEventListener('click', () => {
-        if (!this.properties.playing) this.setTimer('on');
-        this.moveChips(chip);
-      }));
+    this.chips.forEach((chip) => chip.addEventListener('click', () => {
+      if (!this.properties.playing) this.setTimer('on');
+      this.moveChips(chip);
+    }));
   }
 
   getChips() {
     this.chips = Array.from(document.querySelectorAll('.chip'));
-    this.chips.forEach((chip, index) => this.chipsNumbers[index] = chip.textContent);
+    this.chips.forEach((chip, index) => {
+      this.chipsNumbers[index] = chip.textContent;
+    });
   }
 
   fillChips(numbers) {
     this.chipsNumbers = numbers || [];
-    this.chips.forEach((chip, index) => {
+    this.chips.forEach((el, index) => {
+      const chip = el;
       chip.textContent = this.chipsNumbers[index];
       chip.classList = 'chip';
       if (chip.textContent === '') chip.classList.add('chip-empty');
+      return chip;
     });
   }
 
@@ -95,28 +129,51 @@ class Game {
   loadGame() {
     try {
       const savedGame = JSON.parse(localStorage.savedGame);
+      this.chipsNumbers = savedGame.chipsNumbers;
+      this.createChips();
       this.fillChips(savedGame.chipsNumbers);
       this.properties.rows = +localStorage.savedGameRows;
       this.properties.movesCounter = savedGame.properties.movesCounter;
       this.properties.timer = savedGame.properties.timer;
       this.updateHeader();
     } catch (error) {
-      alert('No any saved game found');
+      console.log('No any saved game found');
     }
     if (window.timer) this.setTimer('off');
+
+    this.hideMenu();
   }
 
   randomizeChips() {
     const nums = [];
     const numOfChips = this.properties.rows ** 2 - 1;
-    let generateRandom = () => Math.ceil(Math.random() * numOfChips);
-    for (let i = 0; i < numOfChips; i++) {
+    const generateRandom = () => Math.ceil(Math.random() * numOfChips);
+    for (let i = 0; i < numOfChips; i += 1) {
       let random = generateRandom();
       while (nums.includes(random)) random = generateRandom();
       nums.push(random);
-    };
+    }
     nums.push('');
     return nums;
+  }
+
+  setTimer(switcher) {
+    const tick = () => {
+      this.properties.timer += 1;
+      this.updateHeader();
+    };
+
+    if (switcher === 'on') {
+      this.properties.playing = true;
+      this.header.pauseBtn.textContent = 'Pause game';
+      this.menu.classList.add('hidden');
+      window.timer = window.setInterval(tick, 1000);
+    } else if (switcher === 'off') {
+      this.properties.playing = false;
+      this.header.pauseBtn.textContent = 'Resume game';
+      this.menu.classList.remove('hidden');
+      window.clearInterval(window.timer);
+    }
   }
 
   moveChips(chip) {
@@ -125,10 +182,10 @@ class Game {
     const emptyChip = document.querySelector('.chip-empty');
     const temp = emptyChip.innerHTML;
     const positionDifference = this.chips.indexOf(emptyChip) - chipPos;
-    let chipIsMovable = () => [1, this.properties.rows].includes((Math.abs(positionDifference)));
+    const chipIsMovable = () => [1, this.properties.rows].includes((Math.abs(positionDifference)));
     const params = {
       [`${this.properties.rows}`]: '(0, 100%)',
-      '1': '(100%, 0)',
+      1: '(100%, 0)',
       [`-${this.properties.rows}`]: '(0, -100%)',
       '-1': '(-100%, 0)',
     };
@@ -136,6 +193,7 @@ class Game {
     if (!chipIsMovable()) return;
 
     clickedChip.style.setProperty('transform', `translate${params[positionDifference]}`);
+
     this.updateHeader(1);
 
     setTimeout(() => {
@@ -143,21 +201,22 @@ class Game {
       emptyChip.classList.remove('chip-empty');
       clickedChip.innerHTML = temp;
       clickedChip.classList.add('chip-empty');
-      clickedChip.style.setProperty('transform', `translate(0)`);
+      clickedChip.style.setProperty('transform', 'translate(0)');
       this.getChips();
     }, 150);
   }
 
-  updateHeader(n) {
-    const {
-      timer
-    } = this.properties;
-    let minutes = Math.floor(timer / 60);
+  updateHeader(moves, time = this.properties.timer) {
+    if (time === 0) this.properties.timer = 0;
+    let minutes = Math.floor(time / 60);
     minutes = minutes < 10 ? `0${minutes}` : minutes;
-    let seconds = timer % 60;
+    let seconds = time % 60;
     seconds = seconds < 10 ? `0${seconds}` : seconds;
     document.querySelector('time').innerHTML = `Time: ${minutes}:${seconds}`;
-    document.querySelector('moves').innerHTML = n ? `Moves: ${++this.properties.movesCounter}` : `Moves: ${this.properties.movesCounter}`;
+
+    if (moves === 0) this.properties.movesCounter = 0;
+    if (moves === 1) this.properties.movesCounter += 1;
+    document.querySelector('.moves').innerHTML = `Moves: ${this.properties.movesCounter}`;
   }
 }
 
